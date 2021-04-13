47c923608b7ffe3ba14423b29a05a111ec369496
Lagutov Alexander <lagutov.99@mail.ru>
2021-04-13 20:56:35 +0300

    refactor

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,8 +1,8 @@
-const gulp = require('gulp');
+const gulp = require('gulp')
 
 module.exports = function fonts() {
-	return (
-		gulp.src('src/fonts/**/*').pipe(gulp.dest('build/fonts')),
-		gulp.src('src/assets/**/*').pipe(gulp.dest('build/assets'))
-	);
-};
+    return (
+        gulp.src('src/fonts/**/*').pipe(gulp.dest('build/fonts')),
+        gulp.src('src/assets/**/*').pipe(gulp.dest('build/assets'))
+    )
+}
